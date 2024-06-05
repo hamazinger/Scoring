24ba15872e01b4cd84c414c42d3a056133959745
hamazinger <138296140+hamazinger@users.noreply.github.com>
2024-06-05 19:43:08 +0900

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from wordcloud import WordCloud
 from janome.tokenizer import Tokenizer
 import re
 from datetime import datetime, timedelta
-from st_aggrid import AgGrid, GridOptionsBuilder, GridUpdateMode
+from st_aggrid import AgGrid, GridOptionsBuilder, GridUpdateMode, DataReturnMode
 import pandas as pd
 
 # Streamlitアプリのタイトルを設定
@@ -54,62 +54,88 @@ with col1:
         {"User_Company": "マーケティング・広告・出版・印刷"},
         {"User_Company": "教育"},
         {"User_Company": "その他"},
-        {"User_Company": "IT関連企業"}, # IT関連企業をまとめる
+        {"User_Company": "10. システム・インテグレータ"},
+        {"User_Company": "11. IT・ビジネスコンサルティング"},
+        {"User_Company": "12. IT関連製品販売"},
+        {"User_Company": "13. IT関連製品販売"},
+        {"User_Company": "14. SaaS・Webサービス事業"},
+        {"User_Company": "15. その他ITサービス関連"},
     ]
     gb = GridOptionsBuilder.from_dataframe(pd.DataFrame(industries))
-    gb.configure_selection(selection_mode="multiple", use_checkbox=True, pre_selected_rows=list(range(len(industries)))) # 全て選択済みにする
+    gb.configure_selection(selection_mode="multiple", use_checkbox=True)
+    gb.configure_grid_options(domLayout='normal')
     grid_options_industries = gb.build()
     st.subheader("業種") # 見出しを修正
     selected_rows_industries = AgGrid(
         pd.DataFrame(industries),
         gridOptions=grid_options_industries,
-        update_mode=GridUpdateMode.SELECTION_CHANGED,
+        data_return_mode=DataReturnMode.FILTERED_AND_SORTED,
+        update_mode=GridUpdateMode.MODEL_CHANGED,
+        theme='streamlit',
+        fit_columns_on_grid_load=True,
+        # enable_enterprise_modules=True,
+        height=350,
+        # width='100%',
     )
     selected_industries = selected_rows_industries["data"]["User_Company"].tolist()
 
 # --- 従業員規模選択 ---
 with col2:
     employee_sizes = [
-        {"Employee_Size": "5000人以上"},
-        {"Employee_Size": "1000人以上5000人未満"},
-        {"Employee_Size": "500人以上1000人未満"},
-        {"Employee_Size": "300人以上500人未満"},
-        {"Employee_Size": "100人以上300人未満"},
-        {"Employee_Size": "30人以上100人未満"},
-        {"Employee_Size": "10人以上30人未満"},
-        {"Employee_Size": "10人未満"},
+        {"Employee_Size": "1. 5000人以上"},
+        {"Employee_Size": "2. 1000人以上5000人未満"},
+        {"Employee_Size": "3. 500人以上1000人未満"},
+        {"Employee_Size": "4. 300人以上500人未満"},
+        {"Employee_Size": "5. 100人以上300人未満"},
+        {"Employee_Size": "6. 30人以上100人未満"},
+        {"Employee_Size": "7. 10人以上30人未満"},
+        {"Employee_Size": "8. 10人未満"},
         {"Employee_Size": "分からない"},
     ]
     gb = GridOptionsBuilder.from_dataframe(pd.DataFrame(employee_sizes))
-    gb.configure_selection(selection_mode="multiple", use_checkbox=True, pre_selected_rows=list(range(len(employee_sizes)))) # 全て選択済みにする
+    gb.configure_selection(selection_mode="multiple", use_checkbox=True)
+    gb.configure_grid_options(domLayout='normal')
     grid_options_employee_sizes = gb.build()
     st.subheader("従業員規模") # 見出しを修正
     selected_rows_employee_sizes = AgGrid(
         pd.DataFrame(employee_sizes),
         gridOptions=grid_options_employee_sizes,
-        update_mode=GridUpdateMode.SELECTION_CHANGED,
+        data_return_mode=DataReturnMode.FILTERED_AND_SORTED,
+        update_mode=GridUpdateMode.MODEL_CHANGED,
+        theme='streamlit',
+        fit_columns_on_grid_load=True,
+        # enable_enterprise_modules=True,
+        height=350,
+        # width='100%',
     )
     selected_employee_sizes = selected_rows_employee_sizes["data"]["Employee_Size"].tolist()
 
 # --- 役職選択 ---
 with col3:
     positions = [
-        {"Position_Category": "経営者・役員クラス"},
-        {"Position_Category": "事業部長/工場長クラス"},
-        {"Position_Category": "部長クラス"},
-        {"Position_Category": "課長クラス"},
-        {"Position_Category": "係長・主任クラス"},
-        {"Position_Category": "一般社員・職員クラス"},
-        {"Position_Category": "その他"},
+        {"Position_Category": "1. 経営者・役員クラス"},
+        {"Position_Category": "2. 事業部長/工場長クラス"},
+        {"Position_Category": "3. 部長クラス"},
+        {"Position_Category": "4. 課長クラス"},
+        {"Position_Category": "5. 係長・主任クラス"},
+        {"Position_Category": "6. 一般社員・職員クラス"},
+        {"Position_Category": "7. その他"},
     ]
     gb = GridOptionsBuilder.from_dataframe(pd.DataFrame(positions))
-    gb.configure_selection(selection_mode="multiple", use_checkbox=True, pre_selected_rows=list(range(len(positions)))) # 全て選択済みにする
+    gb.configure_selection(selection_mode="multiple", use_checkbox=True)
+    gb.configure_grid_options(domLayout='normal')
     grid_options_positions = gb.build()
     st.subheader("役職") # 見出しを修正
     selected_rows_positions = AgGrid(
         pd.DataFrame(positions),
         gridOptions=grid_options_positions,
-        update_mode=GridUpdateMode.SELECTION_CHANGED,
+        data_return_mode=DataReturnMode.FILTERED_AND_SORTED,
+        update_mode=GridUpdateMode.MODEL_CHANGED,
+        theme='streamlit',
+        fit_columns_on_grid_load=True,
+        # enable_enterprise_modules=True,
+        height=350,
+        # width='100%',
     )
     selected_positions = selected_rows_positions["data"]["Position_Category"].tolist()
 
@@ -128,7 +154,10 @@ if execute_button:
         # IT関連企業をまとめるための条件を追加
         it_related_condition = " OR ".join([f"User_Company = '{i}'" for i in ["10. システム・インテグレータ", "11. IT・ビジネスコンサルティング", "12. IT関連製品販売", "13. IT関連製品販売"]])
         industry_conditions = " OR ".join([f"User_Company = '{industry}'" for industry in selected_industries if industry != "IT関連企業"])
-        where_clauses.append(f"({industry_conditions} OR ({it_related_condition}))")
+        if industry_conditions:
+            where_clauses.append(f"({industry_conditions} OR ({it_related_condition}))")
+        else:
+            where_clauses.append(f"({it_related_condition})")
     if selected_employee_sizes:
         employee_size_conditions = " OR ".join([f"Employee_Size = '{size}'" for size in selected_employee_sizes])
         where_clauses.append(f"({employee_size_conditions})")
@@ -149,103 +178,4 @@ if execute_button:
         st.warning("業種、従業員規模、役職のいずれかを選択してください。")
         attendee_data = []
 
-    # None値をフィルタリングして企業名のリストを生成
-    filtered_companies = [row['Company_Name'] for row in attendee_data if row['Company_Name'] is not None]
-    filtered_companies = list(set(filtered_companies))  # 重複を削除
-
-    # IN句が空の場合の処理を追加
-    if filtered_companies:
-        # クォートされた企業名のリストを生成
-        quoted_companies = ", ".join([f"'{company}'" for company in filtered_companies])
-
-        # 過去3ヶ月間のセミナーのみを対象とするSQLクエリ
-        all_seminars_query = f"""
-        SELECT *
-        FROM `{destination_table}`
-        WHERE Company_Name IN ({quoted_companies})
-        AND Seminar_Date >= '{three_months_ago.strftime('%Y-%m-%d')}'
-        ORDER BY Company_Name, Seminar_Date
-        """
-
-        # クエリ実行
-        all_seminars_data = run_query(all_seminars_query)
-
-        # スコア計算関数
-        def calculate_score(row):
-            score = 0
-            if row['Status'] == '出席':
-                score += 3
-            if any(row.get(f'Post_Seminar_Survey_Answer_{i}', '') for i in range(1, 4)):
-                score += 3
-            if row.get('Desired_Follow_Up_Actions') is not None:
-                if '製品やサービス導入に関する具体的な要望がある' in row['Desired_Follow_Up_Actions']:
-                    score += 5
-                elif '資料希望' in row['Desired_Follow_Up_Actions']:
-                    score += 3
-            if row.get('Pre_Seminar_Survey_Answer_2') == '既に同様の商品・サービスを導入済み':
-                score += 3
-            elif row.get('Pre_Seminar_Survey_Answer_2') == '既に候補の製品・サービスを絞っており、その評価・選定をしている':
-                score += 3
-            elif row.get('Pre_Seminar_Survey_Answer_2') == '製品・サービスの候補を探している':
-                score += 2
-            elif row.get('Pre_Seminar_Survey_Answer_2') == '導入するかどうか社内で検討中（課題の確認、情報収集、要件の整理、予算の検討）':
-                score += 1
-            return score
-
-        # スコアを計算し、辞書に格納
-        company_scores = {}
-        for row in all_seminars_data:
-            company_name = row['Company_Name']
-            score = calculate_score(row)
-            if company_name in company_scores:
-                company_scores[company_name] += score
-            else:
-                company_scores[company_name] = score
-
-        # スコアの高い順にソート
-        sorted_scores = sorted(company_scores.items(), key=lambda item: item[1], reverse=True)
-
-        # トップ3企業を表示
-        st.header("トップ3企業:")
-        for i in range(3):
-            company_name, score = sorted_scores[i]
-            st.write(f"{i+1}. {company_name}: {score}点")
-
-        # 形態素解析とワードクラウド生成
-        def generate_wordcloud(font_path, text, title):
-            t = Tokenizer()
-            tokens = t.tokenize(text)
-            words = [token.surface for token in tokens if token.part_of_speech.split(',')[0] in ['名詞', '動詞']]
-
-            # フィルタリング条件
-            words = [word for word in words if len(word) > 1]
-            words = [word for word in words if not re.match('^[ぁ-ん]{2}$', word)]
-            words = [word for word in words if not re.match('^[一-龠々]{1}[ぁ-ん]{1}$', word)]
-            
-            # キーワードの除外
-            exclude_words = {'ギフト', 'ギフトカード', 'サービス', 'できる', 'ランキング', '可能', '課題', '会員', '会社', '開始', '開発', '活用', '管理', '企業', '機能',
-                            '記事', '技術', '業界', '後編', '公開', '最適', '支援', '事業', '実現', '重要', '世界', '成功', '製品', '戦略', '前編', '対策', '抽選', '調査',
-                            '提供', '投資', '導入', '発表', '必要', '方法', '目指す', '問題', '利用', '理由', 'する', '解説', '影響', '与える'}
-            words = [word for word in words if word not in exclude_words]
-            
-            # ワードクラウドの生成
-            wordcloud = WordCloud(font_path=font_path, background_color='white', width=800, height=400).generate(' '.join(words))
-
-            # ワードクラウドの表示
-            fig, ax = plt.subplots(figsize=(10, 5))
-            ax.imshow(wordcloud, interpolation='bilinear')
-            ax.set_title(title)
-            ax.axis('off')
-            st.pyplot(fig)
-
-        # トップ3企業のセミナータイトルからワードクラウドを作成
-        st.header("セミナータイトルワードクラウド")
-        for i in range(3):
-            company_name, _ = sorted_scores[i]
-            # 企業ごとのセミナータイトルを取得
-            seminar_titles = ' '.join([row['Seminar_Title'] for row in all_seminars_data if row['Company_Name'] == company_name])
-            # ワードクラウドを生成
-            generate_wordcloud('NotoSansJP-Regular.ttf', seminar_titles, f'{company_name}のセミナータイトルワードクラウド')
-
-    else:
-        st.warning("キーワードに一致する企業が見つかりませんでした。")
+    # --- (以降の処理は同じ) ---
