41942221e2205aa0d3eda83e871844e43446ffa1
hamazinger <138296140+hamazinger@users.noreply.github.com>
2024-06-05 18:06:08 +0900

    Update app.py

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ import matplotlib.pyplot as plt
 import japanize_matplotlib
 from wordcloud import WordCloud
 from janome.tokenizer import Tokenizer
-from datetime import datetime, timedelta
 import re
+from datetime import datetime, timedelta
 
 # Streamlitアプリのタイトルを設定
 st.title("マジセミリードスコアリング＆ワードクラウド")
@@ -29,113 +29,118 @@ def run_query(query):
     return rows
 
 # ユーザーがキーワードを入力できるようにする
-organizer_keyword = st.text_input("主催企業名キーワードを入力してください：", "Aiven")
+organizer_keyword = st.text_input("主催企業名キーワードを入力してください：", "") # 初期値を空にする
 
-# 現在の日付と過去3ヶ月の日付を取得
-today = datetime.today()
-three_months_ago = today - timedelta(days=365)
+# 実行ボタンを追加
+execute_button = st.button("実行")
 
-# マジセミ株式会社が主催したセミナーに参加した企業リストを取得するクエリ
-attendee_query = f"""
-SELECT DISTINCT Company_Name
-FROM `{destination_table}`
-WHERE Organizer_Name LIKE '%{organizer_keyword}%'
-"""
-# クエリ実行
-attendee_data = run_query(attendee_query) # DataFrameに変換しない
+# ボタンが押された場合のみ処理を実行
+if execute_button:
+    # 現在の日付と過去3ヶ月の日付を取得
+    today = datetime.today()
+    three_months_ago = today - timedelta(days=365)
 
-# None値をフィルタリングして企業名のリストを生成
-filtered_companies = [row['Company_Name'] for row in attendee_data if row['Company_Name'] is not None]
-filtered_companies = list(set(filtered_companies)) # 重複を削除
+    # マジセミ株式会社が主催したセミナーに参加した企業リストを取得するクエリ
+    attendee_query = f"""
+    SELECT DISTINCT Company_Name
+    FROM `{destination_table}`
+    WHERE Organizer_Name LIKE '%{organizer_keyword}%'
+    """
+    # クエリ実行
+    attendee_data = run_query(attendee_query) 
 
-# クォートされた企業名のリストを生成
-quoted_companies = ", ".join([f"'{company}'" for company in filtered_companies])
+    # None値をフィルタリングして企業名のリストを生成
+    filtered_companies = [row['Company_Name'] for row in attendee_data if row['Company_Name'] is not None]
+    filtered_companies = list(set(filtered_companies)) # 重複を削除
 
-# 過去3ヶ月間のセミナーのみを対象とするSQLクエリ
-all_seminars_query = f"""
-SELECT *
-FROM `{destination_table}`
-WHERE Company_Name IN ({quoted_companies})
-AND Seminar_Date >= '{three_months_ago.strftime('%Y-%m-%d')}'
-ORDER BY Company_Name, Seminar_Date
-"""
+    # クォートされた企業名のリストを生成
+    quoted_companies = ", ".join([f"'{company}'" for company in filtered_companies])
 
-# クエリ実行
-all_seminars_data = run_query(all_seminars_query) # DataFrameに変換しない
+    # 過去3ヶ月間のセミナーのみを対象とするSQLクエリ
+    all_seminars_query = f"""
+    SELECT *
+    FROM `{destination_table}`
+    WHERE Company_Name IN ({quoted_companies})
+    AND Seminar_Date >= '{three_months_ago.strftime('%Y-%m-%d')}'
+    ORDER BY Company_Name, Seminar_Date
+    """
 
-# スコア計算関数
-def calculate_score(row):
-    score = 0
-    if row['Status'] == '出席':
-        score += 3
-    if any(row.get(f'Post_Seminar_Survey_Answer_{i}', '') for i in range(1, 4)):
-        score += 3
-    if row.get('Desired_Follow_Up_Actions') is not None:
-        if '製品やサービス導入に関する具体的な要望がある' in row['Desired_Follow_Up_Actions']:
-            score += 5
-        elif '資料希望' in row['Desired_Follow_Up_Actions']:
+    # クエリ実行
+    all_seminars_data = run_query(all_seminars_query)
+
+    # スコア計算関数
+    def calculate_score(row):
+        score = 0
+        if row['Status'] == '出席':
             score += 3
-    if row.get('Pre_Seminar_Survey_Answer_2') == '既に同様の商品・サービスを導入済み':
-        score += 3
-    elif row.get('Pre_Seminar_Survey_Answer_2') == '既に候補の製品・サービスを絞っており、その評価・選定をしている':
-        score += 3
-    elif row.get('Pre_Seminar_Survey_Answer_2') == '製品・サービスの候補を探している':
-        score += 2
-    elif row.get('Pre_Seminar_Survey_Answer_2') == '導入するかどうか社内で検討中（課題の確認、情報収集、要件の整理、予算の検討）':
-        score += 1
-    return score
+        if any(row.get(f'Post_Seminar_Survey_Answer_{i}', '') for i in range(1, 4)):
+            score += 3
+        if row.get('Desired_Follow_Up_Actions') is not None:
+            if '製品やサービス導入に関する具体的な要望がある' in row['Desired_Follow_Up_Actions']:
+                score += 5
+            elif '資料希望' in row['Desired_Follow_Up_Actions']:
+                score += 3
+        if row.get('Pre_Seminar_Survey_Answer_2') == '既に同様の商品・サービスを導入済み':
+            score += 3
+        elif row.get('Pre_Seminar_Survey_Answer_2') == '既に候補の製品・サービスを絞っており、その評価・選定をしている':
+            score += 3
+        elif row.get('Pre_Seminar_Survey_Answer_2') == '製品・サービスの候補を探している':
+            score += 2
+        elif row.get('Pre_Seminar_Survey_Answer_2') == '導入するかどうか社内で検討中（課題の確認、情報収集、要件の整理、予算の検討）':
+            score += 1
+        return score
 
-# スコアを計算し、辞書に格納
-company_scores = {}
-for row in all_seminars_data:
-    company_name = row['Company_Name']
-    score = calculate_score(row)
-    if company_name in company_scores:
-        company_scores[company_name] += score
-    else:
-        company_scores[company_name] = score
+    # スコアを計算し、辞書に格納
+    company_scores = {}
+    for row in all_seminars_data:
+        company_name = row['Company_Name']
+        score = calculate_score(row)
+        if company_name in company_scores:
+            company_scores[company_name] += score
+        else:
+            company_scores[company_name] = score
 
-# スコアの高い順にソート
-sorted_scores = sorted(company_scores.items(), key=lambda item: item[1], reverse=True)
+    # スコアの高い順にソート
+    sorted_scores = sorted(company_scores.items(), key=lambda item: item[1], reverse=True)
 
-# トップ3企業を表示
-st.header("トップ3企業:")
-for i in range(3):
-    company_name, score = sorted_scores[i]
-    st.write(f"{i+1}. {company_name}: {score}点")
+    # トップ3企業を表示
+    st.header("トップ3企業:")
+    for i in range(3):
+        company_name, score = sorted_scores[i]
+        st.write(f"{i+1}. {company_name}: {score}点")
 
-# 形態素解析とワードクラウド生成
-def generate_wordcloud(font_path, text, title):
-    t = Tokenizer()
-    tokens = t.tokenize(text)
-    words = [token.surface for token in tokens if token.part_of_speech.split(',')[0] in ['名詞', '動詞']]
+    # 形態素解析とワードクラウド生成
+    def generate_wordcloud(font_path, text, title):
+        t = Tokenizer()
+        tokens = t.tokenize(text)
+        words = [token.surface for token in tokens if token.part_of_speech.split(',')[0] in ['名詞', '動詞']]
 
-    # フィルタリング条件
-    words = [word for word in words if len(word) > 1]
-    words = [word for word in words if not re.match('^[ぁ-ん]{2}$', word)]
-    words = [word for word in words if not re.match('^[一-龠々]{1}[ぁ-ん]{1}$', word)]
-    
-    # キーワードの除外
-    exclude_words = {'ギフト', 'ギフトカード', 'サービス', 'できる', 'ランキング', '可能', '課題', '会員', '会社', '開始', '開発', '活用', '管理', '企業', '機能',
-                     '記事', '技術', '業界', '後編', '公開', '最適', '支援', '事業', '実現', '重要', '世界', '成功', '製品', '戦略', '前編', '対策', '抽選', '調査',
-                     '提供', '投資', '導入', '発表', '必要', '方法', '目指す', '問題', '利用', '理由', 'する', '解説', '影響', '与える'}
-    words = [word for word in words if word not in exclude_words]
-    
-    # ワードクラウドの生成
-    wordcloud = WordCloud(font_path=font_path, background_color='white', width=800, height=400).generate(' '.join(words))
+        # フィルタリング条件
+        words = [word for word in words if len(word) > 1]
+        words = [word for word in words if not re.match('^[ぁ-ん]{2}$', word)]
+        words = [word for word in words if not re.match('^[一-龠々]{1}[ぁ-ん]{1}$', word)]
+        
+        # キーワードの除外
+        exclude_words = {'ギフト', 'ギフトカード', 'サービス', 'できる', 'ランキング', '可能', '課題', '会員', '会社', '開始', '開発', '活用', '管理', '企業', '機能',
+                        '記事', '技術', '業界', '後編', '公開', '最適', '支援', '事業', '実現', '重要', '世界', '成功', '製品', '戦略', '前編', '対策', '抽選', '調査',
+                        '提供', '投資', '導入', '発表', '必要', '方法', '目指す', '問題', '利用', '理由', 'する', '解説', '影響', '与える'}
+        words = [word for word in words if word not in exclude_words]
+        
+        # ワードクラウドの生成
+        wordcloud = WordCloud(font_path=font_path, background_color='white', width=800, height=400).generate(' '.join(words))
 
-    # ワードクラウドの表示
-    fig, ax = plt.subplots(figsize=(10, 5))
-    ax.imshow(wordcloud, interpolation='bilinear')
-    ax.set_title(title)
-    ax.axis('off')
-    st.pyplot(fig)
+        # ワードクラウドの表示
+        fig, ax = plt.subplots(figsize=(10, 5))
+        ax.imshow(wordcloud, interpolation='bilinear')
+        ax.set_title(title)
+        ax.axis('off')
+        st.pyplot(fig)
 
-# トップ3企業のセミナータイトルからワードクラウドを作成
-st.header("セミナータイトルワードクラウド")
-for i in range(3):
-    company_name, _ = sorted_scores[i]
-    # 企業ごとのセミナータイトルを取得
-    seminar_titles = ' '.join([row['Seminar_Title'] for row in all_seminars_data if row['Company_Name'] == company_name])
-    # ワードクラウドを生成
-    generate_wordcloud('NotoSansJP-Regular.ttf', seminar_titles, f'{company_name}のセミナータイトルワードクラウド')
+    # トップ3企業のセミナータイトルからワードクラウドを作成
+    st.header("セミナータイトルワードクラウド")
+    for i in range(3):
+        company_name, _ = sorted_scores[i]
+        # 企業ごとのセミナータイトルを取得
+        seminar_titles = ' '.join([row['Seminar_Title'] for row in all_seminars_data if row['Company_Name'] == company_name])
+        # ワードクラウドを生成
+        generate_wordcloud('NotoSansJP-Regular.ttf', seminar_titles, f'{company_name}のセミナータイトルワードクラウド')
